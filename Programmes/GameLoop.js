bb47b5935ebea785095101b151aee61ca6169a10
Tom <Tom602001@live.fr>
2022-03-26 00:16:10 +0100

    Update GameLoop.js

--- Programmes/GameLoop.js
+++ Programmes/GameLoop.js
@@ -1,4 +1,4 @@
-import Action from "./Action.js";
+import Action from "https://raw.githubusercontent.com/Tom60chat/Cathedrale3D/main/Programmes/Action.js";
 
 export default class GameLoop {
     //region Variables
